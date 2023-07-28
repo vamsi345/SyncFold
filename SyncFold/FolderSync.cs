a77e7e6fc5093ac7f0a81b8167a432a1fe78bbff
vamsi vaddi <vaddivamsi02@gmail.com>
2023-07-28 12:47:43 +0200

    folder sychronisation

---
+++ SyncFold/FolderSync.cs
@@ -0,0 +1,80 @@
+﻿using System.Security.Cryptography;
+using Timer = System.Timers.Timer;
+
+namespace SyncFold
+{
+    public class FolderSync
+    {
+        private string sourceDirectory;
+        private string replicaDirectory;
+        private string logFilePath;
+        private Timer timer;
+
+
+        public FolderSync(string sourceDirectory, string replicaDirectory, string logFilePath, double interval)
+        {
+            this.sourceDirectory = sourceDirectory;
+            this.replicaDirectory = replicaDirectory;
+            this.logFilePath = logFilePath;
+
+            this.timer = new Timer(interval);
+            this.timer.Elapsed += (source, e) => SyncFolders();
+            this.timer.AutoReset = true;
+        }
+
+        public void StartSync()
+        {
+            this.timer.Enabled = true;
+        }
+        private void SyncFolders()
+        {
+            Log("Synchronization started");
+
+            new DirectoryInfo(replicaDirectory).GetFiles()
+     .Where(file => !File.Exists(Path.Combine(sourceDirectory, file.Name)))
+     .ToList().ForEach(file =>
+     {
+         Log($"Deleting {file.Name}");
+         file.Delete();
+     });
+
+            foreach (var file in new DirectoryInfo(sourceDirectory).GetFiles())
+            {
+                string destinationFile = Path.Combine(replicaDirectory, file.Name);
+                if (!File.Exists(destinationFile))
+                {
+                    Log($"Copying {file.Name}");
+                    file.CopyTo(destinationFile, true);
+                }
+                else if (CalculateMD5(file.FullName) != CalculateMD5(destinationFile))
+                {
+                    Log($"Updating {file.Name}");
+                    file.CopyTo(destinationFile, true);
+                }
+            }
+
+            Log("Synchronization completed");
+        }
+        private string CalculateMD5(string filename)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filename))
+                {
+                    byte[] hash = md5.ComputeHash(stream);
+                    return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
+                }
+            }
+        }
+
+        private void Log(string message)
+        {
+            using (StreamWriter writer = new StreamWriter(logFilePath, true))
+            {
+                string logMessage = $"{DateTime.Now}: {message}";
+                writer.WriteLine(logMessage);
+                Console.WriteLine(logMessage);
+            }
+        }
+    }
+}
--- SyncFold/Program.cs
+++ SyncFold/Program.cs
@@ -1,7 +1,25 @@
-﻿public class Program
+﻿using SyncFold;
+public class Program
 {
-   public  static void Main(string[] args)
+    public static void Main(string[] args)
     {
+        if (args.Length != 4)
+        {
+            Console.WriteLine("Usage: FolderSync.exe <source directory> <replica directory> <sync interval in seconds> <log file path>");
+            return;
+        }
 
+        string sourceDirectory = args[0];
+        string replicaDirectory = args[1];
+        double interval = double.Parse(args[2]) * 1000;
+        string logFilePath = args[3];
+
+        FolderSync sync = new FolderSync(sourceDirectory, replicaDirectory, logFilePath, interval);
+        sync.StartSync();
+
+        Console.WriteLine("Press \'q\' to quit the folder synchronization");
+        while (Console.Read() != 'q') ;
     }
+
+
 }
